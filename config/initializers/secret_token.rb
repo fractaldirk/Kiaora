e0f6cbc48fffcb8099daf9168eb5463f1c1e9f65
fractal <circle@ubuntu.(none)>
2013-04-05 21:24:45 +0200

    included cucumber and rspec generators

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kiaora::Application.config.secret_token = '3c851b7b40470c1a79f77d4808e5891ca1c4216d8e8c27828445474a6eca58a0dd326b0f38691c8233063eeac8df1fe3df6d0639f4390cc15d5caef1c682103f'
