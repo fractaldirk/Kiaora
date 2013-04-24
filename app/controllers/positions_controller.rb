5425b26aba6c2dbc8d0c3c9b7e49537c2dc93ad4
fractal <circle@ubuntu.(none)>
2013-04-24 16:19:29 +0200

    unstable release 4.0

--- app/controllers/positions_controller.rb
+++ app/controllers/positions_controller.rb
@@ -156,7 +156,7 @@ class PositionsController < ApplicationController
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @position }
-      format.xls
+      format.xlsx
     end
   end
 end
