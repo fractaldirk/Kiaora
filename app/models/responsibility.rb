78aaa5c4e5dab6502cb1e4a244cfb572f8abb55d
fractaldirk <dirkgrandjean@live.nl>
2013-04-30 22:10:48 +0200

    instable version

--- app/models/responsibility.rb
+++ app/models/responsibility.rb
@@ -1,4 +1,14 @@
 class Responsibility < ActiveRecord::Base
   belongs_to :position
   attr_accessible :content, :indicator, :r_position
+
+  validates_length_of :content_length, :maximum => 255
+
+  def content_length
+    if false
+      errors.add :content, "responsibility too long."
+    end
+  rescue ArgumentError
+    errors.add :content, "is out of range"
+  end
 end
