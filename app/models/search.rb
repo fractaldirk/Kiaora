1150ff34a994713d92545eb0316a3dc159cbc17a
fractaldirk <dirkgrandjean@live.nl>
2013-06-18 15:29:47 +0200

    added filter search form

--- app/models/search.rb
+++ app/models/search.rb
@@ -9,7 +9,7 @@ private
 
   def find_positions
     positions = Position.order(:job_title)
-    positions = Position.where("job_title ilike ?", "%#{keywords}%") if keywords.present?
+    positions = Position.where("job_title like ?", "%#{keywords}%") if keywords.present?
     positions = Position.where(office: office_id) if office_id.present?
     # department code goes here, example: positions = Position.where(department: department) if department.present?
     # works if department is a select box and id field (collection_select) + define model (just like define model for office)
